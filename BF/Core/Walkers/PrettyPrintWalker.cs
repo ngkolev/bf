57fe1405ff8842cb1b40d4ed5c6afd2dd6e8958b
ngkolev <nikolay.kolev@icb.bg>
2015-10-31 15:08:11 +0200

    Add empty pretty print walker

---
+++ BF/Core/Walkers/PrettyPrintWalker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using BF.Core.Abstraction;
+using BF.Core.Expressions;
+
+namespace BF.Core.Walkers
+{
+    class PrettyPrintWalker : IWalker
+    {
+        public void Walk(DataValueDecrementExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Walk(DataValueIncrementExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Walk(DataPointerDecrementExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Walk(DataPointerIncrementExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Walk(WriteCharacterExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Walk(ReadCharacterExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Walk(LoopExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Walk(ProgramExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
