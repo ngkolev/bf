45c5496cce72af9f3f93247ef18ad3a19858af26
ngkolev <ngkolev>
2015-10-31 16:59:18 +0200

    ArgumentParser is implemented

--- BF/ArgumentParser.cs
+++ BF/ArgumentParser.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BF
 {
@@ -10,11 +7,20 @@ namespace BF
     {
         public ArgumentParser(string[] args)
         {
+            Flags = args
+                .Where(a => a.StartsWith("-"))
+                .Select(a => a.Substring(1))
+                .ToList();
+
+            FilePath = args.LastOrDefault(a => !a.StartsWith("-"));
         }
 
-        // TODO: Print help if help flag is on or no filePath is provided
-        public bool ShouldPrintHelp { get; private set; }
-        public bool HasPrettyPrintFlag { get; private set; }
+        private ICollection<string> Flags { get; set; }
+
+        public bool ShouldPrintHelp => Flags.Contains("h") || string.IsNullOrEmpty(FilePath);
+
+        public bool HasPrettyPrintFlag => Flags.Contains("p");
+
         public string FilePath { get; private set; }
     }
 }
